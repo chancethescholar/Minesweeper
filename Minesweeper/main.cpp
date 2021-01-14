8d72449b362439ebfee00485086271329f956d4d
chancethescholar <chanceonyiorah@Chances-MacBook-Pro.local>
2021-01-14 17:47:11 -0500

    first commit

---
+++ Minesweeper/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Awesome Game");
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        
+        while (window.pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    
+                    break;
+            }
+        }
+        
+        window.clear();
+        
+        window.display();
+    }
+}
+
